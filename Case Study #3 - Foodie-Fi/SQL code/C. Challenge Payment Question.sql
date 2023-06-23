a9fbba36cf1e4b0bf909406857eacb4f5c9ef1eb
ElaWajdzik <ela.wajdzik@gmail.com>
2023-06-24 00:49:34 +0200

    Update section C (payments) in CS#3

--- Case Study #3 - Foodie-Fi/SQL code/C. Challenge Payment Question.sql
+++ Case Study #3 - Foodie-Fi/SQL code/C. Challenge Payment Question.sql
@@ -39,74 +39,117 @@ VALUES
 -- plan_id = 3 is a yearly plan, and a payment is one a year
 -- plan_id = 4 it is churn, which is why the start date is the end of payments
 
+-- I don't know yet how to change the price of the new plan.
 
+-- CREATE TABLE payments AS
 
-WITH plan_1 AS (
-SELECT 
-    customer_id,
-    plan_id,
-    DATE(CONCAT('2020','-',n,'-',DAY(start_date))) AS payment_date,
-
-    start_date
-FROM subscriptions
-JOIN n_month
-WHERE 
-    DATE(CONCAT('2020','-',n,'-',DAY(start_date))) >= MONTH(start_date) AND
-    plan_id = 1
-),
-plan_2 AS (
-SELECT 
-    customer_id,
-    plan_id,
-    DATE(CONCAT('2020','-',n,'-',DAY(start_date))) AS payment_date,
-
-    start_date
-FROM subscriptions
-JOIN n_month
-WHERE 
-    n >= MONTH(start_date) AND 
-    plan_id = 2
-),
-plan_3 AS (
-SELECT
-    customer_id,
-    plan_id,
-    start_date AS payment_date,
-
-    start_date
-FROM subscriptions
-WHERE 
-    plan_id = 3
-)
-
-SELECT *
-FROM plan_1
-WHERE customer_id IN (19,2)
-UNION 
-SELECT *
-FROM plan_2
-WHERE customer_id IN (19,2)
-UNION 
-SELECT *
-FROM plan_3
-WHERE customer_id IN (19,2)
-ORDER BY customer_id, start_date, payment_date;
-
-WITH change_plan AS (
+WITH change_plan_id AS (
 SELECT 
     customer_id,
     GROUP_CONCAT(plan_id ORDER BY start_date) AS change_plan,
     GROUP_CONCAT(start_date ORDER BY start_date) AS change_date
 FROM subscriptions
-WHERE customer_id IN (19,21)
-GROUP BY customer_id)
-
+GROUP BY customer_id),
+change_plan_date AS (
 SELECT 
-    *,
-    FIND_IN_SET(plan_id,change_plan)
+    sub.customer_id,
+    sub.plan_id,
+    SUBSTRING(change_date, ((FIND_IN_SET(plan_id,change_plan)-1)*10 + FIND_IN_SET(plan_id,change_plan)), 10) AS start_plan_date,
+    SUBSTRING(change_date, ((FIND_IN_SET(plan_id,change_plan))*10 + FIND_IN_SET(plan_id,change_plan))+1, 10) AS end_plan_date
 FROM subscriptions AS sub
-JOIN change_plan
-    ON change_plan.customer_id = sub.customer_id;
+JOIN change_plan_id
+    ON change_plan_id.customer_id = sub.customer_id),
+change_plan_payments AS (
+SELECT
+    customer_id,
+    plan_id,
+    DATE(start_plan_date) AS start_plan_date,
+    DATE(CASE 
+        WHEN YEAR(DATE(end_plan_date)) >2020 THEN '2020-12-31'
+        WHEN end_plan_date != 0 THEN end_plan_date
+        ELSE '2020-12-31'
+    END) AS end_plan_payments
+FROM change_plan_date
+WHERE plan_id !=0 ANd plan_id !=4),
+
+
+
+plan_1 AS (
+SELECT 
+    customer_id,
+    plan_id,
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) AS payment_date,
+    start_plan_date,
+    end_plan_payments
+FROM change_plan_payments
+JOIN n_month
+WHERE 
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) >= start_plan_date AND
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) < end_plan_payments AND
+    plan_id = 1
+),
+
+plan_2 AS (
+SELECT 
+    customer_id,
+    plan_id,
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) AS payment_date,
+    start_plan_date,
+    end_plan_payments
+FROM change_plan_payments
+JOIN n_month
+WHERE 
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) >= start_plan_date AND
+    DATE(CONCAT('2020','-',n,'-',DAY(start_plan_date))) < end_plan_payments AND
+    plan_id = 2
+),
+
+plan_3 AS (
+SELECT
+    customer_id,
+    plan_id,
+    start_plan_date AS payment_date,
+    start_plan_date,
+    end_plan_payments
+FROM change_plan_payments
+WHERE 
+    plan_id = 3
+)
+    
+SELECT 
+    pay_date.customer_id,
+    pay_date.plan_id,
+    plan_name,
+    pay_date.payment_date,
+    price AS base_price,
+    DENSE_RANK() OVER(
+        PARTITION BY customer_id
+        ORDER BY payment_date
+        ) AS payment_order
+FROM (
+    SELECT *
+    FROM plan_1
+    WHERE customer_id IN (1,2,13,15,16,18,19)
+    
+    UNION
+
+    SELECT *
+    FROM plan_2
+    WHERE customer_id IN (1,2,13,15,16,18,19)
+    
+    UNION
+    
+    SELECT *
+    FROM plan_3
+    WHERE customer_id IN (1,2,13,15,16,18,19)
+    
+    ORDER BY customer_id, payment_date) AS pay_date
+
+JOIN plans
+    ON plans.plan_id = pay_date.plan_id;
+
+
+
 
 SELECT *
 FROM subscriptions
