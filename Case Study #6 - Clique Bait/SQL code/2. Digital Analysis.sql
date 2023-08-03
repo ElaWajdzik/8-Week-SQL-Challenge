dd03e98f6fa1725fc6dac6614cfde359ce39268f
ElaWajdzik <ela.wajdzik@gmail.com>
2023-08-03 23:02:23 +0200

    Update 2. Digital Analysis in CS#6

--- Case Study #6 - Clique Bait/SQL code/2. Digital Analysis.sql
+++ Case Study #6 - Clique Bait/SQL code/2. Digital Analysis.sql
@@ -53,12 +53,84 @@ FROM events AS e,
     FROM events) AS n_visit
 WHERE event_type = 3;
 
+-- 6. What is the percentage of visits which view the checkout page but do not have a purchase event?
 
-/* 
+-- visits which view the checkout page -> page_id = 12
+-- purchase event -> event_type = 3
 
-What is the percentage of visits which have a purchase event?
-What is the percentage of visits which view the checkout page but do not have a purchase event?
-What are the top 3 pages by number of views?
-What is the number of views and cart adds for each product category?
-What are the top 3 products by purchases?
-*/
+-- NOTE: Every purchase requires a visit to the checkout page.
+
+WITH visit_checkout_purchase AS (
+SELECT
+    visit_id,
+    MAX(CASE 
+        WHEN event_type = 3 THEN 1  
+        ELSE 0 
+    END) AS visit_with_purchase,
+    MAX(CASE 
+        WHEN page_id = 12 THEN 1 
+        ELSE 0
+    END) AS visit_with_checkout_page
+FROM events
+GROUP BY visit_id
+)
+
+SELECT
+    SUM(visit_with_checkout_page)-SUM(visit_with_purchase) AS number_of_visit_with_checkout_without_purchase,
+    ROUND(((SUM(visit_with_checkout_page)-SUM(visit_with_purchase))/COUNT(*))*100,1) AS proc_of_visit_with_checkout_without_purchase
+FROM visit_checkout_purchase AS vcp;
+
+
+-- 7. What are the top 3 pages by number of views?
+
+SELECT 
+    e.page_id,
+    ph.page_name,
+    COUNT(e.page_id) AS number_of_viewes
+FROM events AS e, page_hierarchy AS ph
+WHERE e.page_id = ph.page_id
+GROUP BY e.page_id
+ORDER BY COUNT(e.page_id) DESC
+LIMIT 3;
+
+
+-- 8. What is the number of views and cart adds for each product category?
+
+SELECT 
+    ph.product_category,
+    COUNT(e.page_id) AS number_of_viewes,
+        SUM(CASE 
+        WHEN e.event_type = 2 THEN 1
+        ELSE 0
+    END) AS number_of_cart_adds
+FROM events AS e, page_hierarchy AS ph
+WHERE e.page_id = ph.page_id AND ph.product_category IS NOT NULL
+GROUP BY ph.product_category
+ORDER BY COUNT(e.page_id) DESC;
+
+
+-- 9. What are the top 3 products by purchases?
+
+WITH events_with_purchase AS (
+    SELECT 
+        *
+    FROM events
+    WHERE visit_id IN (
+        SELECT
+            visit_id
+        FROM events
+        WHERE event_type = 3)
+)
+
+SELECT 
+    ph.page_name AS product_name,
+    ph.product_category,
+    SUM(CASE 
+        WHEN e.event_type = 2 THEN 1
+        ELSE 0
+    END) AS number_of_buy
+FROM events_with_purchase AS e, page_hierarchy AS ph
+WHERE e.page_id = ph.page_id AND ph.product_category IS NOT NULL
+GROUP BY ph.page_name
+ORDER BY number_of_buy DESC
+LIMIT 3;
