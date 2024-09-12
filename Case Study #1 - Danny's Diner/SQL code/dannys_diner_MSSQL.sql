564e4110749885af06bcc99278a9d3e62e075f5a
ElaWajdzik <ela.wajdzik@gmail.com>
2024-09-12 17:14:19 +0200

    Update dannys_diner_MSSQL.sql
    Add a bonus question

--- Case Study #1 - Danny's Diner/SQL code/dannys_diner_MSSQL.sql
+++ Case Study #1 - Danny's Diner/SQL code/dannys_diner_MSSQL.sql
@@ -3,7 +3,7 @@
 --------------------------------
 
 --Author: Ela Wajdzik
---Date: 9.09.2024
+--Date: 9.09.2024  (update 12.09.2024)
 --Tool used: Microsoft SQL Server
 
 
@@ -251,3 +251,59 @@ INNER JOIN menu mn
 WHERE s.order_date >= m.join_date	--filter the orders placed after becoming a member
 AND s.order_date < '2021-02-01'		--filter the orders at the end of January
 GROUP BY s.customer_id;
+
+
+-------------------
+--Bonus Questions--
+-------------------
+
+--1. Join All The Things
+--Recreate the following table output using the available data
+
+--customer_id   order_date  product_name	price	member
+--A             2021-01-01  curry	        15	    N
+
+SELECT
+	s.customer_id,
+	s.order_date,
+	mn.product_name,
+	mn.price,
+	CASE	
+		WHEN m.join_date <= s.order_date THEN 'Y'	--an additional column with the value 'Y' if the order comes from a customer who is a member, and 'N' if not
+		ELSE 'N'
+	END AS member
+FROM sales s
+INNER JOIN menu mn
+	ON mn.product_id = s.product_id
+LEFT JOIN members m
+	ON m.customer_id = s.customer_id;
+
+
+--2. Rank All The Things
+
+--customer_id	order_date	product_name	price	member	ranking
+--A	            2021-01-01	curry	        15	    N	    null
+
+WITH full_table AS (
+	SELECT
+		s.customer_id,
+		s.order_date,
+		mn.product_name,
+		mn.price,
+		CASE 
+			WHEN m.join_date <= s.order_date THEN 'Y'
+			ELSE 'N'
+		END AS member
+	FROM sales s
+	INNER JOIN menu mn
+		ON mn.product_id = s.product_id
+	LEFT JOIN members m
+		ON m.customer_id = s.customer_id)
+
+SELECT 
+	*,
+	CASE 
+		WHEN member = 'N' THEN null
+		ELSE RANK() OVER (PARTITION BY customer_id, member ORDER BY order_date ASC)		--an additional column with the ranking of orders, but only for orders made by a member
+	END AS ranking
+FROM full_table;
